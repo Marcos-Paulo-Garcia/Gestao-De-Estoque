1e3ef3d560e39da3894e0fec3bc80315ba8e9738
Lucas <lucas.moia@aluno.faculdadeimpacta.com.br>
2025-11-10 21:35:24 -0300

    banco rodando no render database

--- run.py
+++ run.py
@@ -10,13 +10,12 @@ load_dotenv()
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from src.config.data_base import init_db, bcrypt, db, SQLAlchemy
+from src.config.data_base import init_db, bcrypt, db
 from src.routes import init_routes
 
 
 def create_app():
     app = Flask(__name__)
-
     CORS(app)
 
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret-key-default")
@@ -38,7 +37,7 @@ def init_db_command():
     """Cria as tabelas do banco de dados."""
     with app.app_context():
         db.create_all()
-    print("Banco de dados inicializado e tabelas criadas.")
+    print("✅ Banco de dados inicializado e tabelas criadas.")
 
 
 if __name__ == "__main__":
--- src/config/data_base.py
+++ src/config/data_base.py
@@ -8,7 +8,8 @@ db = SQLAlchemy()
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
         'DATABASE_URL',
-        'mysql+pymysql://root:alitheju123@localhost:3306/market_management'
+        'postgresql+psycopg2://market_management_user:pjHE4fCG1m9EMRHXYl4qMiNx7N2lpysy@dpg-d4985g24d50c73969tdg-a/market_management?sslmode=require'
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    print("✅ Banco conectado com sucesso!")
